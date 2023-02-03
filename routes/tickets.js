efdae6316a7d63c9f2e58dcc3bb58d4a5f56a05b
ourgithubacc <111426399+ourgithubacc@users.noreply.github.com>
2023-02-03 18:34:54 +0100

    Add files via upload

---
+++ routes/tickets.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { checkTicket, getAllTickets, getTicketById, deleteTicketById, mailTempToken, getTicketByToken, scan, verifyPassWordForTicket} = require('../controllers/tickets')
+const router = express.Router();
+
+const {protect,userRoleAuth} = require('../middleware/authMiddleware')
+//router.route('/sendEventTicket/:userId').post(sendEventTicket)
+//router.route('/getEventTicket/:userId').get(getEventTicket)
+
+router.route('/tempToken').post(protect,mailTempToken)
+router.route('/getTicketByToken').get(protect,getTicketByToken)
+//router.route('/generateAndSaveTicket/:userId').post(generateAndSaveTicket)
+router.route('/getAllTickets').get(protect,getAllTickets)
+router.route('/getTicketById/:ticketId').get(protect,getTicketById)
+router.route('/checkToken').get(protect,checkTicket)
+router.route('/scan').post(protect,scan)
+router.route('/verifyPassword').post(protect,verifyPassWordForTicket)
+router.route('/deleteTicketById/:ticketId').delete(protect,userRoleAuth,deleteTicketById)
+module.exports = router
