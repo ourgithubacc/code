c07b706fb9d1c8fc7beb0cf97261c7eb0ac92168
ourgithubacc <111426399+ourgithubacc@users.noreply.github.com>
2023-02-12 06:37:26 +0100

    Update tickets.js

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { checkTicket, getAllTickets, getTicketById, deleteTicketById, mailTempToken, getTicketByToken, scan, verifyPassWordForTicket, getTicketByEmail} = require('../controllers/tickets')
+const {getAllTickets, getTicketById, deleteTicketById, mailTempToken, getTicketByToken, scan, verifyPassWordForTicket, getTicketByEmail, generateAndSaveTicket, deleteAll} = require('../controllers/tickets')
 const router = express.Router();
-
 const {protect,userRoleAuth} = require('../middleware/authMiddleware')
+
 
 router.route('/tempToken').post(protect,mailTempToken)
 router.route('/getTicketByToken').get(protect,getTicketByToken)
-router.route('/getAllTickets').get(protect,getAllTickets)
-router.route('/getTicketByEmail/:email').get(protect, getTicketByEmail)
+router.route('/deleteAll').delete(protect,userRoleAuth,deleteAll)
+router.route('/getAllTickets').get(protect,userRoleAuth,getAllTickets)
 router.route('/getTicketById/:ticketId').get(protect,getTicketById)
-router.route('/checkToken').get(protect,checkTicket)
 router.route('/scan').post(protect,scan)
+router.route('/getTicketByEmail/:email').get(protect,getTicketByEmail)
+router.route('/generateAndSaveTicket').post(protect, generateAndSaveTicket)
 router.route('/verifyPassword').post(protect,verifyPassWordForTicket)
 router.route('/deleteTicketById/:ticketId').delete(protect,userRoleAuth,deleteTicketById)
 module.exports = router
