79ea3832e124be7e1b5f67cbf4b2d1635723ec5e
ourgithubacc <111426399+ourgithubacc@users.noreply.github.com>
2023-02-12 06:40:38 +0100

    Update event.js

--- routes/event.js
+++ routes/event.js
@@ -2,11 +2,11 @@ const express =  require('express')
 const {uploadEvent, getAllEvents, getEventByTitle, deleteEvent, updateEvent, getIperuCampusEvents, getMainCampusEvent, deleteAll, deleteAllIperuCampusEvent, deleteAllMainCampusEvent} = require('../controllers/event')
 const router = express.Router();
 const{protect, userRoleAuth} = require('../middleware/authMiddleware')
-const upload = require('../controllers/multer')
-router.route('/uploadEvent',upload.array('eventImage')).post(protect,/*userRoleAuth*/uploadEvent);
+
+
+router.route('/uploadEvent').post(protect,userRoleAuth,uploadEvent);
 router.route('/getAllEvents/:pageNo/:pageSize').get(protect,getAllEvents);
 router.route('/getEventbyTitle').get(protect,getEventByTitle);
-//router.route('/getEventsByHost').get(protect,getEventsByHost);
 router.route('/deleteEvent').delete(protect,userRoleAuth,deleteEvent);
 router.route('/deleteAllMainCampusEvent').delete(protect,userRoleAuth,deleteAllMainCampusEvent)
 router.route('/deleteAllIperuCampusEvent').delete(protect,userRoleAuth,deleteAllIperuCampusEvent)
